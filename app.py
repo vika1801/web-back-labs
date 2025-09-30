0477bf99bc8579a5d5a9f53117d42c542785d888
Вика Сопова <vksspv@gmail.com>
2025-10-01 00:04:11 +0700

    Счётчик

--- app.py
+++ app.py
@@ -1,11 +1,54 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route("/")
-def start():
-    return """<!doctype html>"
-        "<html>"
-        "   <body>"
-        "       <h1>web-сервер на flask</h1>"
-        "   </body>"
-        "</html>"""
+@app.route("/web")
+def web():
+    return """<!doctype html>
+        <html>
+           <body>
+               <h1>web-сервер на flask</h1>
+               <a href="/author">author</a>
+           </body>
+        </html>"""
+
+@app.route("/author")
+def author():
+    name = "Сопова Виктория Андреевна"
+    group = "ФБИ-31"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+        <html>
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Группа: """ + faculty + """</p>
+                <a href="/web">web</a>
+            </body>
+        </html>"""
+
+@app.route('/image')
+def image():
+    path = url_for("static", filename="oak.jpg")
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>Дуб</h1>
+        <img src="'''+'''">
+    </body>
+</html>
+'''
+
+count = 0
+def counter():
+    count += 1
+    return '''
+<!doctype html>
+<html>
+    <body>
+        Сколько раз вы сюда заходили: ''' + count + '''
+    </body>
+</html>
+'''
