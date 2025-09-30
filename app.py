c0e1c8d18c11e28ee5c43596ad019737d49378e4
Вика Сопова <vksspv@gmail.com>
2025-10-01 00:53:48 +0700

    Стили

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request, redirect
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,7 +10,10 @@ def web():
                <h1>web-сервер на flask</h1>
                <a href="/author">author</a>
            </body>
-        </html>"""
+        </html>""", 200, {
+            'X-Server': 'sample'
+            'Content-Type': 'text/plain; charset=utf-8'
+        }
 
 @app.route("/author")
 def author():
@@ -42,13 +45,42 @@ def image():
 '''
 
 count = 0
+
+@app.route('/counter')
 def counter():
-    count += 1
+    global count 
+    time = datetime.datetime.today()
+    url = request.url
+    client_ip = request.remote_addr
+
     return '''
 <!doctype html>
 <html>
     <body>
-        Сколько раз вы сюда заходили: ''' + count + '''
+        Сколько раз вы сюда заходили: ''' + str (count) + '''
+        <hr>
+        Дата и время: ''' + time + '''<br>
+        Запрошенный адрес: ''' + url + '''<br>
+        Ваш IP-адрес: ''' + client_ip + '''<br>
     </body>
 </html>
 '''
+@app.route("/info")
+def info():
+    return redirect("/author")
+
+@app.route("/lab1/created")
+def created():
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>Создано успешно</h1>
+        <div><i>что-то создано...</i></div>
+    </body>
+</html>
+''', 201
+
+@app.errorhandler(404)
+def not_found(err):
+    return "нет такой страницы", 404
